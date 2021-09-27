75fe3ce02a0dd655d97efadf948cc86f4de6108b
rubenlms <73242287+rubenlms@users.noreply.github.com>
2021-09-27 23:31:40 +0100

    Add files via upload

---
+++ es.iespuertodelacruz.rl.lecturaFicheros/src/es/iespuertodelacruz/rl/lecturaficheros/EsIespuertodelacruzRlLecturaFicheros.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.iespuertodelacruz.rl.lecturaficheros;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author dama
+ */
+public class EsIespuertodelacruzRlLecturaFicheros {
+
+    /**
+     * @param args the command line arguments
+     */
+        
+    
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        ArrayList<Persona>listaPersonas = new ArrayList();
+        
+        Path p = Paths.get("personas.txt");
+        
+        BufferedReader br = Files.newBufferedReader(p);
+        
+        String persona;
+
+        
+        while ( (persona = br.readLine()) !=null ){
+        /*
+        while ( (Files.readAllLines(p)) !=null ){
+        */
+            String[]datos = persona.split(";");
+            
+            for (int i = 0; i < datos.length; i++) {
+                Persona p1 = new Persona(datos[0], datos[1], Integer.parseInt(datos[2]));
+                System.out.println("Nombre: " + p1.getNombre() + "\nDNI: " + p1.getDni() + "\nEdad: " + p1.getEdad());
+            }
+            
+        }
+
+    }
+    
+}
---
+++ es.iespuertodelacruz.rl.lecturaFicheros/src/es/iespuertodelacruz/rl/lecturaficheros/Persona.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.iespuertodelacruz.rl.lecturaficheros;
+
+/**
+ *
+ * @author dama
+ */
+public class Persona {
+    
+    String nombre;
+    String dni;
+    int edad;
+
+    public Persona(String nombre, String dni, int edad) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }  
+    
+}
