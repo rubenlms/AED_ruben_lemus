2bd2671775a6af294dd15b4e238e05118b18fe8b
rubenlms <rubenlemushdez@gmail.com>
2021-12-19 14:53:33 +0000

    Desarrollo de AlumnoRest y de MatriculaRest. Correción de errores de mapeo en AlumnoRest.

--- Tema 4/Primera Tarea REST/NewInstituto/src/main/java/es/iespuertodelacruz/rl/controlador/AlumnoRest.java
+++ Tema 4/Primera Tarea REST/NewInstituto/src/main/java/es/iespuertodelacruz/rl/controlador/AlumnoRest.java
@@ -1,11 +1,24 @@
 package es.iespuertodelacruz.rl.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.qos.logback.classic.Logger;
+import es.iespuertodelacruz.rl.entities.Alumno;
 import es.iespuertodelacruz.rl.service.AlumnoService;
 
 @RestController
@@ -18,5 +31,61 @@ public class AlumnoRest {
 	@Autowired
 	AlumnoService alumnoservice;
 	
-	//Implementar métodos (leer pags 39 y ss.)
+	@GetMapping
+	public List<Alumno> getAll(){
+		
+		ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
+		//logger.info("si queremos hacer debug por ejemplo");
+	
+		alumnoservice.findAll().forEach(alum -> alumnos.add((Alumno)alum));
+
+		return alumnos;
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable String id){
+		Optional<Alumno> optM = alumnoservice.findById(id);
+		if(optM.isPresent()) {
+			alumnoservice.deleteById(id);
+			return ResponseEntity.ok("alumno con id:" + id + " borrado");
+		}else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("el id del registro no existe");
+		}
+	}
+	
+	/*
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Alumno alumnoDTO){
+		Alumno alum;
+		alum.setDni(alumnoDTO.getDni());
+		alum.setNombre(alumnoDTO.getNombre());
+		alum.setApellidos(alumnoDTO.getApellidos());
+		alum.setFechanacimiento(alumnoDTO.getFechanacimiento());
+		//falta las matriculas del alumno
+		alumnoservice.save(alum);
+		
+		return ResponseEntity.ok().body(new Alumno(alum));
+	}*/
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable String id,
+	@RequestBody Alumno alumnoDTO){
+		Optional<Alumno> optM = alumnoservice.findById(id);
+
+		if(optM.isPresent()) {
+			Alumno alum = optM.get();
+			alum.setDni(alumnoDTO.getDni());
+			alum.setNombre(alumnoDTO.getNombre());
+			alum.setApellidos(alumnoDTO.getApellidos());
+			alum.setFechanacimiento(alumnoDTO.getFechanacimiento());
+			
+			
+			return ResponseEntity.ok(alumnoservice.save(alum));
+		}else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("el id del registro no existe");
+		}
+	}
+	
+	 
 }
---
+++ Tema 4/Primera Tarea REST/NewInstituto/src/main/java/es/iespuertodelacruz/rl/controlador/MatriculaREST.java
@@ -0,0 +1,87 @@
+package es.iespuertodelacruz.rl.controlador;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.classic.Logger;
+import es.iespuertodelacruz.rl.entities.Matricula;
+import es.iespuertodelacruz.rl.service.MatriculaService;
+
+@RestController
+@RequestMapping("/api/matriculas")
+public class MatriculaREST {
+	
+	private Logger logger =
+			(Logger) LoggerFactory.getLogger(MatriculaREST.class);
+	
+	@Autowired
+	MatriculaService matriculaservice;
+	
+	@GetMapping
+	public List<Matricula> getAll(){
+		
+		ArrayList<Matricula> Matriculas = new ArrayList<Matricula>();
+		//logger.info("si queremos hacer debug por ejemplo");
+	
+		matriculaservice.findAll().forEach(mat -> Matriculas.add((Matricula)mat));
+
+		return Matriculas;
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable int id){
+		Optional<Matricula> optM = matriculaservice.findById(id);
+		if(optM.isPresent()) {
+			matriculaservice.deleteById(id);
+			return ResponseEntity.ok("Matricula con id:" + id + " borrado");
+		}else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("el id del registro no existe");
+		}
+	}
+	
+	/*
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Matricula matriculaDTO){
+		Matricula mat;
+		mat.setIdmatricula(matriculaDTO.getIdmatricula());
+		mat.setYear(matriculaDTO.getYear());
+		//mat.setAlumno(null);
+		matriculaservice.save(mat);
+		
+		return ResponseEntity.ok().body(new Matricula(mat));
+	}*/
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable int id,
+	@RequestBody Matricula matriculaDTO){
+		Optional<Matricula> optM = matriculaservice.findById(id);
+
+		if(optM.isPresent()) {
+			Matricula mat = new Matricula();
+			mat.setIdmatricula(matriculaDTO.getIdmatricula());
+			mat.setYear(matriculaDTO.getYear());
+			//mat.setAlumno(null);
+			
+			
+			return ResponseEntity.ok(matriculaservice.save(mat));
+		}else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("el id del registro no existe");
+		}
+	}
+
+}
